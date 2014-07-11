ef14d8825fa6e4da764c39d2faa3549d83f34c5d
Jim Ockers <jim.ockers@gmail.com>
2014-07-10 20:39:52 -0400

    first commit plot2 script

---
+++ plot2.R
@@ -0,0 +1,47 @@
+## plot2.R
+## creates plot of Global_active_power vs Day for specified date range
+
+startday <- as.Date("2007-02-01")
+endday <- as.Date("2007-02-02")
+
+## read full data file
+## (uncomment for the slow approach, but necessary when changing the
+##  date range, at which point you may study the head and tail of
+##  indices and update index1 and index2 in the faster section below)
+##
+
+# fullData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",
+#           na.strings = "?")
+# DateTime <- strptime(paste(fullData$Date,fullData$Time,sep=" "), "%d/%m/%Y %H:%M:%S")
+# indices <- which(as.Date(DateTime) >= startday & as.Date(DateTime) <= endday)
+# truncData <- fullData[indices,]
+
+## read truncated data file
+## (speedier method, but need to make sure that index1 and index2 are
+##  correctly specified for the date range you wish to study
+##  comment out whole region if you happen to be using the above method)
+
+index1 <- 66637
+index2 <- 69516
+
+cnames <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage",
+              "Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+truncData <- read.table("household_power_consumption.txt", header = FALSE, sep = ";",
+                     na.strings = "?", skip = index1, nrows = index2-index1+1)
+colnames(truncData) <- cnames
+DateTime <- strptime(paste(truncData$Date,truncData$Time,sep=" "), "%d/%m/%Y %H:%M:%S")
+
+## assign data types to columns in data frame
+
+truncData$Date <- as.Date(truncData$Date, format = "%d/%m/%Y")
+
+## Plot histogram
+
+plot(DateTime, truncData$Global_active_power,
+     type = "l",
+     ylab = "Global Active Power (kilowatts)" )
+
+# Direct a copy to png file
+
+dev.copy(png, file = "plot2.png")
+dev.off()
